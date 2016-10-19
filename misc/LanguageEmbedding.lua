f9e832d5eb337d1f4c44cce8319a80aee829da7f
Jiasen Lu <echosenm@gmail.com>
2016-10-19 17:10:19 -0400

    Update LanguageEmbedding.lua

--- misc/LanguageEmbedding.lua
+++ misc/LanguageEmbedding.lua
@@ -66,7 +66,7 @@ function LanguageEmbedding.conv(conv_size,embedding_size, seq_length)
 
     local unigram_dim = nn.View(seq_length, conv_size, 1):setNumInputDims(3)(unigram)
     local bigram_dim = nn.View(seq_length, conv_size, 1):setNumInputDims(3)(bigram_pad)
-    local trigram_dim = nn.View(seq_length, conv_size, 1):setNumInputDims(3)(trigram_pads)
+    local trigram_dim = nn.View(seq_length, conv_size, 1):setNumInputDims(3)(trigram_pad)
 
     local feat = nn.JoinTable(4)({unigram_dim, bigram_dim, trigram_dim})
     local max_feat = nn.Dropout(0.5)(nn.Max(3, 3)(feat))
